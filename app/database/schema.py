e73981834e0eff6e1ef7f2cc811e728f1d2c2388
Eden Kang <edenkang5517@gmail.com>
2024-01-01 22:48:08 +0900

    fix : key- > id 변경

--- app/database/schema.py
+++ app/database/schema.py
@@ -5,16 +5,16 @@ from database.base import Base
 class User(Base):
     __tablename__ = "user"
 
-    user_key = Column(INTEGER, primary_key=True, nullable=False, autoincrement=True)
+    user_id = Column(INTEGER, primary_key=True, nullable=False, autoincrement=True)
     user_name = Column(String(30), nullable=False)
 
 
 class Channel(Base):
     __tablename__ = "channel"
 
-    channel_key = Column(INTEGER, primary_key=True, nullable=False, autoincrement=True)
+    channel_id = Column(INTEGER, primary_key=True, nullable=False, autoincrement=True)
     channel_name = Column(String(30), nullable=False)
-    channel_creator = Column(INTEGER, nullable=False)
+    channel_creator_id = Column(INTEGER, nullable=False)
     channel_user_count = Column(INTEGER, nullable=False, default=0)
     channel_check_type = Column(String(10), nullable=False)
     updated_at = Column(DateTime, default=func.current_timestamp())
@@ -24,8 +24,8 @@ class Channel(Base):
 class Check(Base):
     __tablename__ = "check"
 
-    check_key = Column(INTEGER, primary_key=True, nullable=False, autoincrement=True)
-    check_channel_key = Column(INTEGER, nullable=False)
-    check_user_key = Column(INTEGER, nullable=False)
+    check_id = Column(INTEGER, primary_key=True, nullable=False, autoincrement=True)
+    check_channel_id = Column(INTEGER, nullable=False)
+    check_user_id = Column(INTEGER, nullable=False)
     updated_at = Column(DateTime, default=func.current_timestamp())
     created_at = Column(DateTime, default=func.current_timestamp())
