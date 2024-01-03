b1d211676be4a241bd19373fd712e7707afb8264
EdenKangdw <dongwon.kang@lomin.ai>
2024-01-04 00:44:54 +0900

    feat : 채널 생성 2

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SocialKakao from "./page/socialKakao";
 import ChannelLobby from "./page/channelLobby";
 import Channel from "./page/channel";
+import CreateChannel from "./page/createChannel";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<SocialKakao />} />
         <Route path="/main" element={<ChannelLobby />} />
         <Route path="/channel" element={<Channel />} />
+        <Route path="/channel/create" element={<CreateChannel />} />
       </Routes>
     </Router>
   );
