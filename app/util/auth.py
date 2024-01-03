11ff5ee95296138e93d1a43eb6d4ae45c1977479
EdenKangdw <dongwon.kang@lomin.ai>
2024-01-03 15:25:00 +0900

    fix : auth - db 세션 처리 5

--- app/util/auth.py
+++ app/util/auth.py
@@ -54,7 +54,7 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     return encoded_jwt
 
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], session: Session = Depends(db.session)):
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], session: Annotated[Session, Depends(db.session)]):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
